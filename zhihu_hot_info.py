e0fdd65348f93f2207302399e8040c2b55de5803
yxy2829 <yxy2829@gmail.com>
2018-05-02 16:09:42 +0800

    update

--- zhihu_hot_info.py
+++ zhihu_hot_info.py
@@ -12,12 +12,10 @@ db = client.zhihu_db
 headers = {
 	# 'Cookie': 'q_c1=92c243e2be934a46be193b721a7640e1|1525241530000|1525241530000; capsion_ticket=2|1:0|10:1525241439|14:capsion_ticket|44:NjAzNDAxM2VmYzQ5NDlkMTk0OWJiY2IxYTI1ZjJiNzU=|2cea3eff2509ee18e74a19644f7924d98fdf9b5cff3ade938f3f685ae318b845; aliyungf_tc=AQAAAPFGJhXaegAAApb4cpjRP/23YZHq; z_c0=2|1:0|10:1525241484|4:z_c0|92:Mi4xZy0wakJ3QUFBQUFBMEtCOEdvMTdEUXdBQUFCZ0FsVk5qT01RV3dDUXhWSXY4anU1a3dyS2FSVFREeEk5ZnM4OXln|a67bf045207c49267ea6650da053819ad4e5d6294e44a2d033a6ef62bfc635a1; unlock_ticket=AGDhIelJ7gwMAAAAYAJVTZRd6VrW4hj2EBFEwi2fmCdc_woCetd-cw==',
 	'User-Agent': 'com.zhihu.android/Futureve/5.14.2 Mozilla/5.0 (Linux; Android 4.4.2; OPPO R11 Build/NMF26X) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36',
-	'Authorization': 'Bearer 2.1g-0jBwAAAAAA0KB8Go17DQwAAABgAlVNjOMQWwCQxVIv8ju5kwrKaRTTDxI9fs89yg',
+	'Authorization': 'your Authorization',
 }
 
 def get_info(url):
-	# url = 'https://api.zhihu.com/topstory/hot-list?limit=10'
-
 	r = requests.get(url, headers=headers)
 	j = r.json()
 	# pprint(j)
@@ -48,23 +46,13 @@ def get_info(url):
 	collection.insert_many(data_list)
 	return question_id_list
 
-"""
- 'paging': {'is_end': False,
-            'is_start': True,
-            'next': 'https://api.zhihu.com/questions/275021771/answers?limit=10&offset=10',
-            'previous': 'https://api.zhihu.com/questions/275021771/answers?limit=10&offset=0',
-            'totals': 7199}}
-"""
 
 def get_answer(url):
-	# url = 'https://api.zhihu.com/questions/{}/answers?limit=10&offset=0'.format(question_id)
-	# https://www.zhihu.com/question/275360633/answer/380321597
 	r = requests.get(url, headers=headers)
 	time.sleep(random.random() * 1.5)
 	j = r.json()
 	all_answer = j.get('data')
 
-	# answer_list = []
 	for answer in all_answer:
 		d = dict()
 		pprint(answer)
@@ -82,14 +70,7 @@ def get_answer(url):
 		# d['answer_url'] = answer.get('url')
 		d['updated_time'] = answer.get('updated_time')
 		d['answer_id'] = answer.get('id')
-		d['answer_url'] = 'https://www.zhihu.com/question/{}/answer/{}'.format(d['question_id'], 
-			d['answer_id'])
-		# pprint(d)
-		# answer_list.append(d)
-
-		# collection_name = 'question_' + str(d['question_id'])
-		# collection = db[collection_name]
-		# collection.insert_one(d)
+		d['answer_url'] = 'https://www.zhihu.com/question/{}/answer/{}'.format(d['question_id'], d['answer_id'])
 		yield d
 
 	paging = j.get('paging')
@@ -102,15 +83,10 @@ def get_answer(url):
 def get_answer_data(d):
 	answer_url = d.get('answer_url')
 	time.sleep(random.random() * 1.5)
-	# 'https://api.zhihu.com/answers/378472913'
-	# https://www.zhihu.com/question/{}/answer/{}
 	r = requests.get(answer_url, headers=headers)
-
 	# file_name = '_'.join(answer_url.split('/')[-4:]) + '.html'
 	# with open(file_name, 'wb') as f:
 	# 	f.write(r.content)
-	# j = r.json()
-	# pprint(j)
 	html = etree.HTML(r.text)
 	d['upvote_count'] = html.xpath('//meta[@itemprop="upvoteCount"]/@content')
 	d['comment_count'] = html.xpath('//meta[@itemprop="commentCount"]/@content')
@@ -121,19 +97,16 @@ def get_answer_data(d):
 	collection_name = 'question_' + str(d['question_id'])
 	collection = db[collection_name]
 	collection.insert_one(d)
-	print('-'*30)
 
 
 def main():
 	index_url = 'https://api.zhihu.com/topstory/hot-list'
-	# https://api.zhihu.com/questions/275360633/answers?order_by=&offset=5
 	q_url = 'https://api.zhihu.com/questions/{}/answers?order_by=&offset=0'
 	question_id_list = get_info(index_url)
 	for q_id in question_id_list:
 		full_q_url = q_url.format(q_id)
 		for d in get_answer(full_q_url):
 			get_answer_data(d)
-
 
 
 if __name__ == '__main__':
